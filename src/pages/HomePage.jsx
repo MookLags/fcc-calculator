56eb8196c3e887cad60eafdcc49eb0886969d57e
Mitchell Lagaras <mookielagaras@gmail.com>
2025-03-17 11:41:57 -0400

    began writing functions for CalcButton components. working on operators

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,20 +4,10 @@ import { useState, useEffect } from 'react';
 
       {/*
        Functions: 
-       handleNums
-        display numbers and decimal
-        if currVal == 0, disable 0 button
-        if decimalPlaced == true, disable decimal button
        handleOperators
-        reset currVal to whatever operator is clicked
-        operatorSet == true
-        if operatorSet == true and currVal == "-" then append operator to expression and operatorSet == false
-        if operatorSet == true and currVal == any number, append operator to expression and operatorSet == false
        handleEquals
         if expression == null, "/", "*", "+", "-" and equals is pressed, result = NaN
         if pressed, result = eval(expression), append `=${result}` to expressionDisplay, and setCurrVal(result)
-       handleClear
-        setCurrval("0") setExpression("")
       */}
 
 const HomePage = () => {
@@ -52,39 +42,67 @@ const HomePage = () => {
   }
 
   const sayHi = () => { console.log("hi") };
-
+  
   const handleNums = (num) => {
-    {/* if currVal == 1-9 => append num to currVal
-        else currVal = num
-    */}
-    if (currVal == /[1-9]/) {
-      setCurrVal(prevVal => prevVal + num)
+    if (currVal.toString().length == 12) {
+      return;
+    }
+
+    if (currVal != 0 && operatorSet == false && currVal.toString().length <= 12 || currVal == "0." ) {
+      setCurrVal(prevVal => prevVal + num);
     } else {
       setCurrVal(num);
     }
   }
 
+  const handleDecimal = () => {
+    if (decimalSet) {
+      return;
+    }
+    setCurrVal(prevVal => prevVal + ".");
+    setDecimalSet(true);
+  }
+
+  const handleOperators = (op) => {
+    setExpression(currVal);
+    setCurrVal(op)
+    setOperatorSet(true);
+    setDecimalSet(false);
+  }
+
+  const handleClear = () => {
+    setCurrVal("0");
+    setExpression("");
+    setDecimalSet(false);
+  }
+
+
+        {/* reset currVal to whatever operator is clicked
+        operatorSet == true
+        if operatorSet == true and currVal == "-" then append operator to expression and operatorSet == false
+        if operatorSet == true and currVal == any number, append operator to expression and operatorSet == false*/}
+
   return (
     <div id="calculator" style={calcStyle}>
       <Screen displayText={currVal} expressionText={expression}/>
       <div id="buttonContainer" style={{ display: "flex", flexDirection: "row", marginLeft: "auto", marginRight: "auto", flexWrap: "wrap"}}>
-        <CalcButton value="AC" bgColor="#ac3939" buttonWidth="160px" func={sayHi} />
-        <CalcButton value="/" bgColor="#666666" func={sayHi} />
-        <CalcButton value="*" bgColor="#666666" func={sayHi} />
+        <CalcButton value="AC" bgColor="#ac3939" buttonWidth="160px" func={handleClear} />
+        <CalcButton value="/" bgColor="#666666" func={() => handleOperators("/")} />
+        <CalcButton value="*" bgColor="#666666" func={() => handleOperators("*")} />
         <CalcButton value="7" func={() => {handleNums("7")}} />
         <CalcButton value="8" func={sayHi} />
         <CalcButton value="9" func={sayHi} />
-        <CalcButton value="-" bgColor="#666666" func={sayHi} />
+        <CalcButton value="-" bgColor="#666666" func={() => handleOperators("-")} />
         <CalcButton value="4" func={sayHi} />
         <CalcButton value="5" func={sayHi} />
         <CalcButton value="6" func={sayHi} />
-        <CalcButton value="+" bgColor="#666666" func={sayHi} />
+        <CalcButton value="+" bgColor="#666666" func={() => handleOperators("+")} />
         <CalcButton value="1" func={sayHi} />
         <CalcButton value="2" func={sayHi} />
         <CalcButton value="3" func={sayHi} />
         <CalcButton value="=" bgColor="#004466" buttonHeight="130px" isRelative={false} func={sayHi} />
-        <CalcButton value="0" buttonWidth="160px" func={sayHi} />
-        <CalcButton value="." buttonWidth="79px" func={sayHi} />
+        <CalcButton value="0" buttonWidth="160px" func={() => {handleNums("0")}} />
+        <CalcButton value="." buttonWidth="79px" func={handleDecimal} />
       </div>
     </div>
   )
